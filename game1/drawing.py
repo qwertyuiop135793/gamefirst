515e5cf9404914cfc22650a8fdf8e7240bbff8ff
qwertyuiop135792 <yaroslavstepanov@mail.ru>
2026-09-03 20:34:50 +0500

    update

--- game1/drawing.py
+++ game1/drawing.py
@@ -1,3 +1,5 @@
+import math
+
 from game1.map import world_map
 from map import mini_map
 from settings import *
@@ -8,15 +10,20 @@ class Drawing:
         self.sc = sc
         self.sc_map = sc_map
         self.font = pygame.font.SysFont('Arial', 36, bold=True)
-        self.texture = pygame.image.load('textures/wall1.png').convert()
-    def background(self):
-        pygame.draw.rect(self.sc, SKYBLUE, (0, 0, WIDTH, HEIGHT_HALF))
+        self.textures = {'1':pygame.image.load('textures/wall1.png').convert(),
+                        '2':pygame.image.load('textures/wall2.png').convert(),
+                        's':pygame.image.load('textures/sky.png').convert()}
+    def background(self, angle):
+        sky_offset = -10 * math.degrees(angle) % WIDTH
+        self.sc.blit(self.textures['s'], (sky_offset, 0))
+        self.sc.blit(self.textures['s'], (sky_offset - WIDTH, 0))
+        self.sc.blit(self.textures['s'], (sky_offset + WIDTH, 0))
         pygame.draw.rect(self.sc, DARKGREY, (0, 400, WIDTH, HEIGHT_HALF))
     def world(self, player_pos, player_angle):
-        ray_casting(self.sc, player_pos, player_angle, self.texture)
+        ray_casting(self.sc, player_pos, player_angle, self.textures)
     def fps(self, clock):
         display_fps = str(int(clock.get_fps()))
-        render = self.font.render(display_fps, 0, RED)
+        render = self.font.render(display_fps, 0, DARKGORANGE)
         self.sc.blit(render, (WIDTH - 65, 5))
     def mini_map(self, player):
         self.sc_map.fill(BLACK)
@@ -25,5 +32,5 @@ class Drawing:
         pygame.draw.line(self.sc_map, YELLOW, (map_x,map_y), (map_x + 12 * math.cos(player.angle),
                                                                map_y + 12 * math. sin(player.angle)), 2)
         for x, y in mini_map:
-            pygame.draw.rect(self.sc_map, GREEN, (x, y, MAP_TILE, MAP_TILE))
+            pygame.draw.rect(self.sc_map, DARKGROWN, (x, y, MAP_TILE, MAP_TILE))
         self.sc.blit(self.sc_map, MAP_POS)
