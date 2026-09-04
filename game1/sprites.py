ac77aac1e85260b88edd1f5c8e64cc78a983e3ad
qwertyuiop135793 <rrrrrrrrr.rcycyvubib@mail.ru>
2026-09-04 21:16:27 +0500

    Update sprites.py

---
+++ game1/sprites.py
@@ -0,0 +1,22 @@
+import pygame
+from settings import *
+
+
+
+class Sprites:
+    def __init__(self):
+        self. sprite_types = {
+            'barrel':pygame.image.load('sprites/images/BAR1E0.png').convert_alpha()
+        }
+        self.list_of_objects = [
+            SpriteObject(self.sprite_types['barrel'], True, (7.1, 2.1), 1.8, 0.4)
+        ]
+class SpriteObject:
+    def __init__(self, object, static, pos, shift, scale):
+        self.object = object
+        self.static = static
+        self.pos = self.x, self.y = pos[0] * TILE, pos[1] * TILE
+        self.shift = shift
+        self.scale = scale
+    def object_locate(self, player, walls):
+        pass
