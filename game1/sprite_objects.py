ccf3690f8b6a70ef233350491a22033af5a08330
qwertyuiop135793 <rrrrrrrrr.rcycyvubib@mail.ru>
2026-09-05 12:26:53 +0500

    Update and rename sprites.py to sprite_objects.py

--- game1/sprite_objects.py
+++ game1/sprite_objects.py
@@ -9,7 +9,8 @@ class Sprites:
             'barrel':pygame.image.load('sprites/barrel/0.png').convert_alpha()
         }
         self.list_of_objects = [
-            SpriteObject(self.sprite_types['barrel'], True, (7.1, 2.1), 1.8, 0.4)
+            SpriteObject(self.sprite_types['barrel'], True, (7.1, 2.1), 1.8, 0.4),
+            SpriteObject(self.sprite_types['barrel'], True, (5.9, 2.1), 1.8, 0.4)
         ]
 class SpriteObject:
     def __init__(self, object, static, pos, shift, scale):
